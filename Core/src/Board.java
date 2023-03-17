13cb4e9a468759b835270f0d5e352aec2dcca01c
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-17 21:48:12 +0100

    Added drawing board functionality

---
+++ Core/src/Board.java
@@ -0,0 +1,11 @@
+public class Board {
+
+    public static void pirntGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
