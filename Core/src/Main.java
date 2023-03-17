99aec729c1e909afc267daef7fa6d5cf4c6c4be1
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-17 22:36:51 +0100

    Tweeking winining conditions

--- Core/src/Main.java
+++ Core/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -11,46 +12,74 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Board.pirntGameBoard(gameBoard);
 
-        System.out.println("Wpisz numer pola, w którym chcesz wpisać kółko albo krzyżyk ");
-        int pos= scanner.nextInt();
 
+
+
+        Player player1 = new Player( "Gracz pierwszy");
+        Player player2 = new Player("Gracz drugi");
+        Player player = player1;
+
+        char symbol;
         char cross = '\u274c';
-        char circle= '\u25ef';
+        char circle= '\u2b24';
+        int pos;
+        Random random = new Random();
 
 
 
+do {
 
-        switch (pos){
-            case 1:
-                gameBoard[0][0] = cross;
-                break;
-            case 2:
-                gameBoard[0][2] = cross ;
-                break;
-            case 3:
-                gameBoard[0][4] = cross;
-                break;
-            case 4:
-                gameBoard[2][0] = cross;
-                break;
-            case 5:
-                gameBoard[2][2] = cross;
-                break;
-            case 6:
-                gameBoard[2][4] = cross;
-                break;
-            case 7:
-                gameBoard[4][0] = cross;
-                break;
-            case 8:
-                gameBoard[4][2] = cross;
-                break;
-            case 9:
-                gameBoard[4][4] = cross;
-                break;
-        }
-        Board.pirntGameBoard(gameBoard);
 
+
+    if (player.equals(player1)){ symbol = cross;
+        System.out.println("Wybierz polę, w którym zamierzasz umieścić symbol");
+        pos= scanner.nextInt();
+    }
+
+    else { symbol = circle;
+        pos = random.nextInt(9);
+
+    }
+do {
+    switch (pos) {
+        case 1:
+            gameBoard[0][0] = symbol;
+            break;
+        case 2:
+            gameBoard[0][2] = symbol;
+            break;
+        case 3:
+            gameBoard[0][4] = symbol;
+            break;
+        case 4:
+            gameBoard[2][0] = symbol;
+            break;
+        case 5:
+            gameBoard[2][2] = symbol;
+            break;
+        case 6:
+            gameBoard[2][4] = symbol;
+            break;
+        case 7:
+            gameBoard[4][0] = symbol;
+            break;
+        case 8:
+            gameBoard[4][2] = symbol;
+            break;
+        case 9:
+            gameBoard[4][4] = symbol;
+            break;
+    } while (gameBoard. != cross || circle);
+    }
+
+
+    if (player == player1) {player = player2;
+    }
+    else {player = player1;
+        Board.pirntGameBoard(gameBoard);}
+
+
+} while (true);
     }
 
 }
---
+++ Core/src/WinerChecker.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WinerChecker {
+
+    public static  String checkWinner (){
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midleRow = Arrays.asList(4, 5, 6);
+        List bottomRow = Arrays.asList(7, 8, 9);
+
+        List<List> allWiningConditions = new ArrayList<List>();
+        allWiningConditions.add(topRow);
+        allWiningConditions.add(midleRow);
+        allWiningConditions.add(bottomRow);
+
+        if (p)
+
+        return "";
+    }
+}
