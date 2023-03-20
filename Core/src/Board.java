2436a418a44b3317037a0e05990e92d9649cc1a2
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-20 19:46:39 +0100

    drawing board

--- Core/src/Board.java
+++ Core/src/Board.java
@@ -1,14 +1,22 @@
 import java.util.Arrays;
 
 public class Board {
-    public static char row[] = {' ', '|', ' ', '|', ' '};
-    public static char planc[] = {'-', '+', '-', '+', '-'};
-    public static char[][] board = {row, row, row};
+
+    public static char[][] board = {
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}};
 
 
 
     public static void drawBoard(){
-
+        for (int i = 0; i < 5; i++ ){
+            for (int y = 0; y <5; y++){
+                System.out.println(i);
+            }
+        }
         }
     }
-}
+
