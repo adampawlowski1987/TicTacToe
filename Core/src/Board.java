9581037ee4c5ca210e90432981968baafa4fa469
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-20 19:37:39 +0100

    drawing board

--- Core/src/Board.java
+++ Core/src/Board.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class Board {
+    public static char row[] = {' ', '|', ' ', '|', ' '};
+    public static char planc[] = {'-', '+', '-', '+', '-'};
+    public static char[][] board = {row, row, row};
+
 
 
     public static void drawBoard(){
-        System.out.println("działam");
+
+        }
     }
 }
