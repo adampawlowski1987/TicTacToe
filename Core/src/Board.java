a024dbf4138128a952e7e4ba00a494444228511c
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-20 21:05:29 +0100

    drawing board funtionality added

--- Core/src/Board.java
+++ Core/src/Board.java
@@ -1,22 +1,47 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Board {
 
+
     public static char[][] board = {
+
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '}};
 
-
-
-    public static void drawBoard(){
-        for (int i = 0; i < 5; i++ ){
-            for (int y = 0; y <5; y++){
-                System.out.println(i);
+    public static void drawBoard() {
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c);
             }
-        }
+            System.out.println();
         }
     }
+    public static void addPosition(char[][] board) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj numer pozycji, w której chcesz wpisać symbol");
+
+       int  playerPosition = scanner.nextInt();
+
+       switch (playerPosition){
+           case 1:  board[0][0] = 'x'; break;
+           case 2:  board[0][2] = 'x'; break;
+           case 3:  board[0][4] = 'x'; break;
+           case 4:  board[2][0] = 'x'; break;
+           case 5:  board[2][2] = 'x'; break;
+           case 6:  board[2][4] = 'x'; break;
+           case 7:  board[4][0] = 'x'; break;
+           case 8:  board[4][2] = 'x'; break;
+           case 9:  board[4][4] = 'x'; break;
+           default: break;
+       }
+    }
+}
+
 
--- Core/src/Main.java
+++ Core/src/Main.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Integer> userPositions = new ArrayList<Integer>();
+
+//        userPositions = Arrays.stream(Board.board).toArray();
 
         Board.drawBoard();
 
-
+        while (true) {
+            Board.addPosition(Board.board);
+            Board.drawBoard();
+        }
 
     }
 }
