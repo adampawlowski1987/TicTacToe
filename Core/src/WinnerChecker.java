c09164b63b75a3dcc147a55c47d684a01a446968
adampawlowski1987 <adampawlowski1987@gmail.com>
2023-03-20 22:13:05 +0100

    Check winner function tweeking, another change x2

--- Core/src/WinnerChecker.java
+++ Core/src/WinnerChecker.java
@@ -8,7 +8,17 @@ public class WinnerChecker {
 
     public static void checkWinner(ArrayList<Integer> userPosition){
         List topRow = Arrays.asList(1, 2, 3);
-        if (userPosition.contains(topRow)){
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+
+
+        List<List> fullList = new ArrayList<List>();
+        fullList.add(topRow);
+        fullList.add(midRow);
+        fullList.add(botRow);
+
+        if (userPosition.containsAll(fullList))
+        for (List i :fullList){
         System.out.println("wygrałeś");}
 else{
             System.out.println("");
